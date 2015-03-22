8ef70dc75f4f2d6b43e14e855571b7fd0247465e
jacquelinelibert <j.libert@student.rug.nl>
2015-03-22 21:23:38 +0000

    straight path

--- Matlab/expe_options.m
+++ Matlab/expe_options.m
@@ -2,7 +2,7 @@ function options = expe_options(options)
 
     if is_test_machine
         options.result_path   = '~/resultsBeautiful/Gender';
-        options.straight_path = '../lib/STRAIGHTV40_006b';
+        options.straight_path = '../lib/STRAIGHTV40_006b'; 
     else
         [~, name] = system('hostname');
         if strncmp(name, '12-000-4372', 11) % PT's computer
@@ -12,19 +12,12 @@ function options = expe_options(options)
             options.sound_path = 'C:/Users/Jacqueline Libert/Documents/Github/Gender/Stimuli/equalized';
             options.tmp_path = 'C:/Users/Jacqueline Libert/Documents/Github/Gender/Stimuli/processed';
             options.straight_path = 'C:/Users/Jacqueline Libert/Documents/GitHub/BeautifulFishy/lib/STRAIGHTV40_006b';
-<<<<<<< HEAD
-=======
             options.result_path   = '../results';
-            
->>>>>>> 98b426090e19803b1ff6648db1818294a7a37b06
         end
     end
 
-    options.result_prefix = 'gen_';
-<<<<<<< HEAD
+%     options.result_prefix = 'gen_';
 
-=======
->>>>>>> 98b426090e19803b1ff6648db1818294a7a37b06
     
     % The current status of the experiment, number of trial and phase, is
     % written in the log file. Ideally this file should be on the network so
