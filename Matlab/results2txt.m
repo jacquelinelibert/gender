62795b3009c21d9053e4b74b00668ed650202a64
paolotof <varati@hotmail.com>
2015-05-08 16:48:30 +0200

    added condition to file exporting results to file

--- Matlab/results2txt.m
+++ Matlab/results2txt.m
@@ -1,8 +1,12 @@
 
-fileID = fopen('allResults.txt', 'wt');
-fprintf(fileID,'subID\tphase\tf0\tvtl\tface\tword\thands\tbutton\tRT \n');
+% cd('/home/paolot/results/gender/');
+cd('/home/paolot/Dropbox/Results/Gender/');
 
-cd('C:/Users/Jacqueline Libert/Documents/GitHub/Results/Gender/Result files');
+fileID = fopen('allResults.txt', 'wt');
+fprintf(fileID,'subID\tphase\tf0\tvtl\tface\tword\thands\tbutton\tRT\tIsWoman \n');
+% fileAve = fopen('meanResults.txt', 'wt');
+% fprintf(fileAve,'subID\tphase\tf0\tvtl\tface\tword\thands\tbutton\tRT \n');
+
 
 files = dir('*.mat');
 nFiles = length(files);
@@ -24,12 +28,31 @@ for ifiles = 1:nFiles
             fprintf(fileID,'%s\t', results.(phases{iphase}).responses(iresp).trial.face);
             fprintf(fileID,'%s\t', results.(phases{iphase}).responses(iresp).trial.word);
             fprintf(fileID,'%s\t', results.(phases{iphase}).responses(iresp).trial.hands);
-            
             fprintf(fileID,'%i\t', results.(phases{iphase}).responses(iresp).button_clicked);
             fprintf(fileID,'%02.3f\t', results.(phases{iphase}).responses(iresp).response_time);
+            % button_clicked = 1 -> yes; button_clicked = 2 -> no; 
+            % female = 1;
+            % isWoman = 1; % face = man, resp 'no' + face = woman resp = 'yes'
+            % isWoman = 1; % face = man, resp 2 + face = woman resp = 1
+            fprintf(fileID,'%i\t', (results.(phases{iphase}).responses(iresp).trial.face(1) == 'm' && ...
+                results.(phases{iphase}).responses(iresp).button_clicked == 2) || ...
+                (results.(phases{iphase}).responses(iresp).trial.face(1) == 'w' && ...
+                results.(phases{iphase}).responses(iresp).button_clicked == 1));
+            
+            
             fprintf(fileID,'\n');
         end
     end
+    
+%     fprintf(fileID,'%s\t', ppID);
+%     fprintf(fileID,'%s\t', phases{iphase});
+
+    
+    
+%     fprintf(fileID,'%02.3f\t', results.(phases{iphase}).responses(iresp).response_time);
+
+    
 end
 
 fclose(fileID);
+% fclose(fileAve);
